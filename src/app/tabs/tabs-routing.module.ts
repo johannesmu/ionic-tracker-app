5bf831a0a8ef7e4fc32f0b3c1ee2aab8b8bcd4b0
johannes <johannes.muljana@ait.nsw.edu.au>
2019-11-13 14:58:09 +1100

    added timer to tracker page

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,43 +8,12 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
-      },
-      {
         path: 'tracker',
         children: [
           {
             path: '',
             loadChildren: () => 
               import('../tracker/tracker.module').then( m => m.TrackerPageModule)
-            
           }
         ]
       },
--- src/app/tracker/tracker.page.ts
+++ src/app/tracker/tracker.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracker',
@@ -6,17 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracker.page.scss'],
 })
 export class TrackerPage implements OnInit {
+  taskForm:FormGroup;
   started:Boolean = false;
-  constructor() { }
+  startTime:number;
+  stopTime:number;
+
+  timerSub:Subscription;
+  time:number;
+
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+     name: ['', [Validators.required, Validators.minLength(3) ] ] 
+    });
   }
 
   start() {
     this.started = true;
+    this.startTime = new Date().getTime();
+    const tm = timer(0,1000);
+    this.timerSub = tm.subscribe( val => this.time = val );
+    console.log( this.startTime );
   }
 
   stop() {
     this.started = false;
+    this.stopTime = new Date().getTime();
+    this.timerSub.unsubscribe();
+    console.log( this.stopTime );
   }
 }
